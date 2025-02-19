0f0ae3116df6953a533fa9ef6fd88825ad74026b
marian4cost4 <mariana.costa.lisboa5579@gmail.com>
2025-02-19 12:40:08 -0300

    pages routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css'
 import Home from './components/home'
+import Alunos from './components/alunos'
+import {BrowserRouter, Routes, Route,Link} from 'react-router-dom'
 
 function App() {
   return(<>
-    <Home/>
+    <BrowserRouter>
+      <ul>
+        <li><Link to={"/"}>Home</Link></li>
+        <li><Link to={"/alunos"}>Cadastrar Alunos</Link></li>
+      </ul>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/alunos' element={<Alunos/>}/>
+      </Routes>
+    </BrowserRouter>
   </>)
 }
 
