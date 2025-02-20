99afa850d5b934fb5cce82e4bc1f6fcdeb7c8085
marian4cost4 <mariana.costa.lisboa5579@gmail.com>
2025-02-20 09:58:00 -0300

    create db in .json and table styling

--- src/components/alunos/alunos.js
+++ src/components/alunos/alunos.js
@@ -1,9 +1,42 @@
 import styles from "./alunos.module.css"
 
-function Alunos(){
-    return(<main className={styles.container}>
-        <h1>Página de execução</h1>
-    </main>)
+import React from "react"
+
+class Alunos extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            alunos : []
+        }
+    }
+
+    render(){
+        return(<main className={styles.container}>
+            <h1>Página de execução</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Opções</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Teste</th>
+                        <th>teste@teste.com</th>
+                        <th>Atualizar ou Excluir</th>
+                    </tr>
+                    <tr>
+                        <th>Teste</th>
+                        <th>teste@teste.com</th>
+                        <th>Atualizar ou Excluir</th>
+                    </tr>
+                </tbody>
+            </table>
+        </main>)
+    }
 }
 
 export default Alunos
