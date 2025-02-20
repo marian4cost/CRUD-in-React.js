3aa61065f62392e0264499a3a9fc86d5cf7e942f
marian4cost4 <mariana.costa.lisboa5579@gmail.com>
2025-02-20 11:09:41 -0300

    using state from manul way

--- src/components/alunos/alunos.js
+++ src/components/alunos/alunos.js
@@ -7,7 +7,10 @@ class Alunos extends React.Component{
         super(props)
 
         this.state = {
-            alunos : []
+            alunos : [
+                {"id": 0, "name": "mariana", "email": "mariana.costa.lisboa078@gmail.com"},
+                {"id": 1, "name": "joão", "email": "joão@teste.com"}
+            ]
         }
     }
 
@@ -17,20 +20,15 @@ class Alunos extends React.Component{
             <table>
                 <thead>
                     <tr>
-                        <th>Nome</th>
-                        <th>E-mail</th>
-                        <th>Opções</th>
+                        <th>NOME</th>
+                        <th>E-MAIL</th>
+                        <th>OPÇÕES</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <th>Teste</th>
-                        <th>teste@teste.com</th>
-                        <th>Atualizar ou Excluir</th>
-                    </tr>
-                    <tr>
-                        <th>Teste</th>
-                        <th>teste@teste.com</th>
+                        <th>{this.state.alunos[0].name}</th>
+                        <th>{this.state.alunos[0].email}</th>
                         <th>Atualizar ou Excluir</th>
                     </tr>
                 </tbody>
